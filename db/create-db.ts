23ec17913d35ba3efe7e4eb21af3af1e319082fc
wuzb <wuzhenbing@gmail.com>
2020-01-16 00:02:11 +0900

    db commit

--- db/create-db.ts
+++ db/create-db.ts
@@ -6,11 +6,12 @@ export class CreateDb {
         const db = DbUtils.DbInstance;
         db.serialize(
             () => {
-                db.run('CREATE TABLE IF NOT EXISTS summaryTestInfo (id INTEGER, title TEXT, createdDate TEXT, score INTEGER, costTime INTEGER, createdDate TEXT)');
+                db.run('CREATE TABLE IF NOT EXISTS summaryTestInfo (id INTEGER, title TEXT, score INTEGER, costTime INTEGER, createdDate TEXT)');
                 db.run('CREATE TABLE IF NOT EXISTS summaryDetailInfo (id INTEGER, summaryTestId INTEGER, contentId INTEGER, contentResId INTEGER)');
                 db.run('CREATE TABLE IF NOT EXISTS mainInfo (id INTEGER, title TEXT, createdDate TEXT, summary INTEGER)');
                 db.run('CREATE TABLE IF NOT EXISTS contentInfo (id INTEGER,  mainId INTEGER, typeINTEGER, content TEXT, createddate TEXT , baseTime INTEGER)');
                 db.run('CREATE TABLE IF NOT EXISTS contentResInfo (id INTEGER, contentId INTEGER, res INTEGER, createddate TEXT)');
+                db.run('CREATE TABLE IF NOT EXISTS textBookInfo (id INTEGER, title TEXT, courseCount INTEGER)');
             }
         )
         db.close();
