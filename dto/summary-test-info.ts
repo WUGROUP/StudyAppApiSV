e727a0c5c268a3c24f0c566bc8f266924cc1731f
wuzb <wuzhenbing@gmail.com>
2020-01-26 21:41:59 +0900

    commit

--- dto/summary-test-info.ts
+++ dto/summary-test-info.ts
@@ -1,5 +1,8 @@
+import { MainInfo } from "./main-info";
+
 export class SummaryTestInfoDto {
     public id!: number;
+    public mainInfos!: MainInfo[];
     public title!: string;
     public score!: number;
     public costTime!: number;
